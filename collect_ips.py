23647c721e011e20dea0cbe5fdccd6537a817f63
makochan2022 <117690357+makochan2022@users.noreply.github.com>
2025-08-12 23:00:59 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -103,7 +103,7 @@ with open('ip.txt', 'w') as file:
     if unique_ipv4:
         sorted_ipv4 = sorted(unique_ipv4, key=lambda ip: [int(part) for part in ip.split('.')])
         for index, ip in enumerate(sorted_ipv4, start=1):
-            file.write(f"{ip}:2053#CF优选节点{index:02d}\n")
+            file.write(f"{ip}:2053#TLS优选节点TLS{index:02d}\n")
         print(f"Saved {len(sorted_ipv4)} unique IPv4 addresses to ip.txt.")
     else:
         print("No valid IPv4 addresses found, creating empty ip.txt.")
@@ -112,7 +112,7 @@ with open('ipv4notls.txt', 'w') as file:
     if unique_ipv4:
         sorted_ipv4 = sorted(unique_ipv4, key=lambda ip: [int(part) for part in ip.split('.')])
         for index, ip in enumerate(sorted_ipv4, start=1):
-            file.write(f"{ip}:2052#CF优选节点{index:02d}\n")
+            file.write(f"{ip}:2052#NOTLS优选节点{index:02d}\n")
         print(f"Saved {len(sorted_ipv4)} unique IPv4 addresses to ipv4notls.txt.")
     else:
         print("No valid IPv4 addresses found, creating empty ipv4notls.txt.")
@@ -122,7 +122,7 @@ with open('ipv6.txt', 'w') as file:
     if unique_ipv6:
         sorted_ipv6 = sorted(unique_ipv6)
         for index, ip in enumerate(sorted_ipv6, start=1):
-            file.write(f"[{ip}]:2053#CF优选节点{index:02d}\n")
+            file.write(f"[{ip}]:2053#TLS优选节点{index:02d}\n")
         print(f"Saved {len(sorted_ipv6)} unique IPv6 addresses to ipv6.txt.")
     else:
         print("No valid IPv6 addresses found, creating empty ipv6.txt.")
@@ -131,7 +131,7 @@ with open('ipv6notls.txt', 'w') as file:
     if unique_ipv6:
         sorted_ipv6 = sorted(unique_ipv6)
         for index, ip in enumerate(sorted_ipv6, start=1):
-            file.write(f"[{ip}]:2052#CF优选节点{index:02d}\n")
+            file.write(f"[{ip}]:2052#NOTLS优选节点{index:02d}\n")
         print(f"Saved {len(sorted_ipv6)} unique IPv6 addresses to ipv6notls.txt.")
     else:
         print("No valid IPv6 addresses found, creating empty ipv6notls.txt.")
